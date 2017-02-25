212177a3f066f0d517088c9ed16cce9c76b7aea8
Tr3jer_CongRong <CongRong@MacBook-Pro.local>
2017-02-25 16:22:36 +0800

    更新sina、xiaomi、antgroup

---
+++ result_import.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+#from scan result import here
+
+import os
+
+result_path = 'down/'
+save_path = 'IncExtensiveList/'
+
+for i in os.listdir(result_path):
+    save_tmp = set()
+
+    if os.path.isdir(result_path+i):
+        for j in os.listdir(result_path+i):
+            result_tmp = result_path+i+'/'+j+'/extensive_ip.txt'
+            if os.path.isfile(result_tmp):
+                for f in open(result_tmp):
+                    save_tmp.add(f.strip())
+
+    if save_tmp:
+        save_file = save_path + i + '.txt'
+        try:
+            os.remove(save_file)
+        except:
+            pass
+        finally:
+            f = open(save_file,'a')
+            for i in save_tmp:
+                f.write(i+'\n')
+            f.close()
